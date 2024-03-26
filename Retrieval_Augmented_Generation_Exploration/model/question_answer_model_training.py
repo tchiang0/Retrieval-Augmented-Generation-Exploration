f1e7eaf4f406c671a0028887b6120189c0783f7a
Diane Chiang <tchiang0@uw.edu>
2024-03-25 19:54:17 -0700

    Added qa model training

---
+++ Retrieval_Augmented_Generation_Exploration/model/question_answer_model_training.py
@@ -0,0 +1,86 @@
+from transformers import AutoTokenizer, DefaultDataCollator, AutoModelForQuestionAnswering, TrainingArguments, Trainer
+from datasets import load_dataset
+import json
+
+
+movie_dataset = load_dataset("csv", data_files={"train": "train.csv", "validation": "val.csv"})
+tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
+
+def preprocess_function(examples):
+    questions = [q.strip() for q in examples["question"]]
+    inputs = tokenizer(
+        questions,
+        examples["context"],
+        max_length=512,
+        truncation="only_second",
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    offset_mapping = inputs.pop("offset_mapping")
+    answers = examples["answers"]
+    start_positions = []
+    end_positions = []
+
+    for i, offset in enumerate(offset_mapping):
+        answer = json.loads(answers[i])
+        start_char = answer["answer_idx"][0]
+        end_char = answer["answer_idx"][1]
+        sequence_ids = inputs.sequence_ids(i)
+
+        # Find the start and end of the context
+        idx = 0
+        while sequence_ids[idx] != 1:
+            idx += 1
+        context_start = idx
+        while sequence_ids[idx] == 1:
+            idx += 1
+        context_end = idx - 1
+
+        # If the answer is not fully inside the context, label it (0, 0)
+        if offset[context_start][0] > end_char or offset[context_end][1] < start_char:
+            start_positions.append(0)
+            end_positions.append(0)
+        else:
+            # Otherwise it's the start and end token positions
+            idx = context_start
+            while idx <= context_end and offset[idx][0] <= start_char:
+                idx += 1
+            start_positions.append(idx - 1)
+
+            idx = context_end
+            while idx >= context_start and offset[idx][1] >= end_char:
+                idx -= 1
+            end_positions.append(idx + 1)
+
+    inputs["start_positions"] = start_positions
+    inputs["end_positions"] = end_positions
+    return inputs
+
+tokenized_squad = movie_dataset.map(preprocess_function,
+                                    batched=True,
+                                    remove_columns=movie_dataset["train"].column_names)
+
+data_collator = DefaultDataCollator()
+model = AutoModelForQuestionAnswering.from_pretrained("distilbert/distilbert-base-uncased")
+training_args = TrainingArguments(
+    output_dir="movie_qa_model",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    push_to_hub=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_squad["train"],
+    eval_dataset=tokenized_squad["validation"],
+    tokenizer=tokenizer,
+    data_collator=data_collator,
+)
+
+trainer.train()
